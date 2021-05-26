e2afbced305743d8c690df8acd0a44f5aff214c2
JanuaryII <450627420@QQ.COM>
2021-05-26 14:37:41 +0800

    githubtest

--- src/App.js
+++ src/App.js
@@ -24,65 +24,6 @@ const onFinish = (values) => {
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
-// const Demo = () => {
-//   const onFinish = (values) => {
-//     console.log('Success:', values);
-//   };
-
-//   const onFinishFailed = (errorInfo) => {
-//     console.log('Failed:', errorInfo);
-//   };
-
-//   return (
-//     <Form
-//       {...layout}
-//       name="basic"
-//       initialValues={{
-//         remember: true,
-//       }}
-//       onFinish={onFinish}
-//       onFinishFailed={onFinishFailed}
-//     >
-//       <Form.Item
-//         label="Username"
-//         name="username"
-//         rules={[
-//           {
-//             required: true,
-//             message: 'Please input your username!',
-//           },
-//         ]}
-//       >
-//         <Input />
-//       </Form.Item>
-
-//       <Form.Item
-//         label="Password"
-//         name="password"
-//         rules={[
-//           {
-//             required: true,
-//             message: 'Please input your password!',
-//           },
-//         ]}
-//       >
-//         <Input.Password />
-//       </Form.Item>
-
-//       <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-//         <Checkbox>Remember me</Checkbox>
-//       </Form.Item>
-
-//       <Form.Item {...tailLayout}>
-//         <Button type="primary" htmlType="submit">
-//           Submit
-//         </Button>
-//       </Form.Item>
-//     </Form>
-//   );
-// };
-
-// ReactDOM.render(<Demo />, mountNode);
 
 function App() {
     return (
